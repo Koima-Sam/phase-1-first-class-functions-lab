a37eb1858f4a20159b51250f8199946dd4792a66
Koima-Sam <sam17koima@gmail.com>
2022-06-08 22:28:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+//Returns first two drivers
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+//returns last two drivers
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+//Allows us invoke either function from an array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//Takes fare as an argument and returns a function that quadruples it
+function createFareMultiplier(value){
+    return function(fare){
+        return fare * value;
+    }
+}
+// fuction that doubles the fare
+const fareDoubler = createFareMultiplier(2);
+
+//function that triples the fare
+const fareTripler = createFareMultiplier(3);
+
+//returns the first two drivers when passed returnFirstTwoDrivers() as the second argument
+function selectDifferentDrivers(drivers, selectedDrivers){
+    return selectedDrivers(drivers);
+
+}
